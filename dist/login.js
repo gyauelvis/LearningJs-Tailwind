b70e87f2a852c9ab82f84ed5a7abdb66ddf3bb3a
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2023-01-15 22:52:22 +0000

    Initial commit

---
+++ dist/login.js
@@ -0,0 +1,9 @@
+const formVerification = () => {
+  let document
+  let emailRegex = /.+@.+.\..+/;
+  let digitRegex = /[0 - 9]/;
+  let smallLetterRegex = /[a-z]/;
+  let caps = /[A-Z]/;
+  let symbolRegex = /[!@#$%^&*(),.?":{}|_<>]/;
+
+};
