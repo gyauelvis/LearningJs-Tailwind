0445e937b0a46fe7eb784b8f976482a490862cac
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2023-01-17 14:39:50 +0000

    17thJan

--- dist/login.js
+++ dist/login.js
@@ -1,9 +1,25 @@
 const formVerification = () => {
-  let document
   let emailRegex = /.+@.+.\..+/;
-  let digitRegex = /[0 - 9]/;
+  let digitRegex = /[0-9]/;
   let smallLetterRegex = /[a-z]/;
-  let caps = /[A-Z]/;
+  let capsRegex = /[A-Z]/;
   let symbolRegex = /[!@#$%^&*(),.?":{}|_<>]/;
+  let getPasswordValue = document.querySelector("input[type='password']").value;
+  let passwordHelp = document.querySelector(".passwordHelp");
+  console.log(digitRegex.test(getPasswordValue));
+  console.log(smallLetterRegex.test(getPasswordValue));
+  console.log(capsRegex.test(getPasswordValue));
+  console.log(symbolRegex.test(getPasswordValue));
 
+  if (
+    digitRegex.test(getPasswordValue) &&
+    smallLetterRegex.test(getPasswordValue) &&
+    capsRegex.test(getPasswordValue) &&
+    symbolRegex.test(getPasswordValue)
+  ) {
+    passwordHelp.style.display = "none";
+  } else {
+    passwordHelp.style.display = "block";
+  }
 };
+document.querySelector(".submit").addEventListener("click", formVerification);
