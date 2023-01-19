5748dcc4c5adebd5a0347ebf66ceeae69c8f1a70
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2023-01-19 23:11:51 +0000

    20th Jan

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        bermuda: "#78dcca",
+      },
+    },
   },
   plugins: [],
 };
